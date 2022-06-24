712821e29036b3560e6e21ee57846ed128bb2b49
Idaly-Rodran <20183ti180@utez.edu.mx>
2022-06-24 10:46:12 -0500

    first commit

---
+++ src/main/java/mx/edu/utez/aweb/pokemonapp/service/Ability/ServiceAbility.java
@@ -0,0 +1,4 @@
+package mx.edu.utez.aweb.pokemonapp.service.Ability;
+
+public class ServiceAbility {
+}
---
+++ src/main/java/mx/edu/utez/aweb/pokemonapp/service/Pokemon/ServicePokemon.java
@@ -0,0 +1,4 @@
+package mx.edu.utez.aweb.pokemonapp.service.Pokemon;
+
+public class ServicePokemon {
+}
